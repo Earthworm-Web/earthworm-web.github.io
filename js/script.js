6c37d37fedea2acf15190c6a071ff38ccc07a9e8
Little-Earthworm <3547347506@qq.com>
2020-09-19 21:38:58 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+x=document.getElementById("news");
+x.innerHTML="<marquee><span>News: Not yet</span></marquee>";
+
+x=document.getElementById("wnews");
+x.innerHTML="<marquee class="watch"><span>News: Not yet</span></marquee>";
