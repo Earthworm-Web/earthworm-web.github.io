c7e1dd7ded8acf275cd454ffabdc25f464501310
Little-Earthworm <3547347506@qq.com>
2020-09-20 22:34:24 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,1 @@
 document.getElementById("news").innerHTML="<marquee><span>News: Not yet!</span></marquee>";
-
-document.getElementById("wnews").innerHTML="<marquee class='watch'><span>News: Not yet!</span></marquee>";
