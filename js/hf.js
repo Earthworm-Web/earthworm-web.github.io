7fd4283bd962d0d1cf67d432c940eeb89cf70232
Little-Earthworm <3547347506@qq.com>
2020-10-02 17:51:00 +0800

    Update hf.js

--- js/hf.js
+++ js/hf.js
@@ -1,3 +1,3 @@
-document.getElementById("head").innerHTML="<ul class='head'><img src='/favicon.ico'width='40'height='40'><li><a href='/'>Home</a></li><li><a href='/en-us/index'>En-US</a></li><li><a href='/zh-cn/index'>Zh-CN</a></li><li><a href='/test'>Test</a></li><li><a href='/movies/BZHF.mp4'>BZHF</a></li></ul><marquee><span>News: Update'ing</span></marquee>";
+document.getElementById("head").innerHTML="<ul class='head'><img src='/favicon.ico'width='40'height='40'><li><a href='/'>Home</a></li><li><a href='/en-us/index'>En-US</a></li><li><a href='/zh-cn/index'>Zh-CN</a></li><li><a href='/test'>Test</a></li><li><a href='/movies/BZHF.mp4'>BZHF</a></li><li><marquee><span>News: Update'ing</span></marquee></li></ul>";
 
 document.getElementById("foot").innerHTML="<hr><p align='center'>Oct 1, 2020 | ©2020 <a href='https://github.com/Little-Earthworm' target='_blank'>Little-Earthworm</a></p>";
