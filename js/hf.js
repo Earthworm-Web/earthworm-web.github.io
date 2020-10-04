7c2d56b8639deb49a1e645ab941f10a8a1e0cfe4
Little-Earthworm <3547347506@qq.com>
2020-10-04 22:49:02 +0800

    Update hf.js

--- js/hf.js
+++ js/hf.js
@@ -1,3 +1,3 @@
-document.getElementById("head").innerHTML="<ul class='head'><li><a href='/'>Home</a></li><li><a href='/projects'>Projects</a></li><li>Updating!</li><marquee><p>News: Updating</p></marquee></ul>";
+document.getElementById("head").innerHTML="<ul class='head'><li><a href='/'>Home</a></li><li><a href='/projects'>Projects</a></li><marquee><p>News: Updating</p></marquee></ul>";
 
 document.getElementById("foot").innerHTML="<hr><p class='date'>Oct 4, 2020 | ©2020 <a href='https://github.com/Little-Earthworm' target='_blank'>Little-Earthworm</a></p>";
