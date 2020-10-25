d1d6350c9e5c1910be307b0c5d2ba594dff9b146
Little-Earthworm <3547347506@qq.com>
2020-10-25 17:14:56 +0800

    💥 Update Countdown

--- js/countdown.js
+++ js/countdown.js
@@ -1,22 +1,22 @@
-document.getElementById("countdown").innerHTML="<p><b>Jumping to the Home. (5s)</b></p><p>正在跳转到主页。（5秒）";
+document.getElementById("countdown").innerHTML="<div id='center'><p><b>Jumping to the Home. (5s)</b></p><p>正在跳转到主页。（5秒）</div>";
 setTimeout("t4()","1000");
 function t4() {
-    document.getElementById("countdown").innerHTML="<p><b>Jumping to the Home. (4s)</b></p><p>正在跳转到主页。（4秒）";
+    document.getElementById("countdown").innerHTML="<div id='center'><p><b>Jumping to the Home. (4s)</b></p><p>正在跳转到主页。（4秒）</div>";
     setTimeout("t3()","1000");
 }
 function t3() {
-    document.getElementById("countdown").innerHTML="<p><b>Jumping to the Home. (3s)</b></p><p>正在跳转到主页。（3秒）";
+    document.getElementById("countdown").innerHTML="<div id='center'><p><b>Jumping to the Home. (3s)</b></p><p>正在跳转到主页。（3秒）</div>";
     setTimeout("t2()","1000");
 }
 function t2() {
-    document.getElementById("countdown").innerHTML="<p><b>Jumping to the Home. (2s)</b></p><p>正在跳转到主页。（2秒）";
+    document.getElementById("countdown").innerHTML="<div id='center'><p><b>Jumping to the Home. (2s)</b></p><p>正在跳转到主页。（2秒）</div>";
     setTimeout("t1()","1000");
 }
 function t1() {
-    document.getElementById("countdown").innerHTML="<p><b>Jumping to the Home. (1s)</b></p><p>正在跳转到主页。（1秒）";
+    document.getElementById("countdown").innerHTML="<div id='center'><p><b>Jumping to the Home. (1s)</b></p><p>正在跳转到主页。（1秒）</div>";
     setTimeout("t0()","1000");
 }
 function t0() {
-    document.getElementById("countdown").innerHTML="<p><b>Jumping to the Home. (0s)</b></p><p>正在跳转到主页。（0秒）";
+    document.getElementById("countdown").innerHTML="<div id='center'><p><b>Jumping to the Home. (0s)</b></p><p>正在跳转到主页。（0秒）</div>";
     window.location.href = '/';
 }
