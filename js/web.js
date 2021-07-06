e2af54d4f1ecef509d9cf4a1739e73c682770668
Quinn0823 <3547347506@qq.com>
2021-07-06 14:02:47 +0800

    🕳 Repair TIPS‘ Bug

--- js/web.js
+++ js/web.js
@@ -1,5 +1,13 @@
-/*Little-Earthworm provides this technology. ©2020 Little-Earthworm*/
+/*Quinn0823 provides this technology. ©2021 Quinn0823*/
 
 if (top != self) {  
     location.href = "/nested";  
+} else {
+    setTimeout(tip(), 0);
+    function tip(){
+        var tip=confirm("⚠ Warning:\nEarthworm-Web is no longer updated.\n\n🔗 compare:\nQuinn-Web is more beautiful than Earthworm-Web.\nEarthworm-Web is richer than Quinn-Web.\n\nClick 'OK' to go to Quinn-Web, and click 'Cancel' to stay here.");
+        if (tip == true){
+            window.location=("https://quinn0823.github.io");
+        }
+    }
 }
